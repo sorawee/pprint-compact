31da65bb921634da5877510f8fbffd8b36e4bfec
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2021-10-11 11:31:42 -0700

    add build-deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,9 @@
 #lang info
 (define collection "pprint-compact")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("at-exp-lib"
+                     "data-enumerate-lib"
+                     "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/pprint-compact.scrbl" ())))
 (define pkg-desc "An implementation of 'A Pretty But Not Greedy Printer'")
 (define version "0.0")
