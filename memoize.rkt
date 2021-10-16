457558285e9f9f488c9265d2fe52e8b43ef04543
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2021-10-16 10:16:20 -0700

    slightly faster, but mainly reduce memory pressure

--- memoize.rkt
+++ memoize.rkt
@@ -2,10 +2,10 @@
 
 (provide memoize memoize2)
 
-(define (memoize f #:backend [backend make-hasheq])
+(define (memoize f #:backend [backend make-weak-hasheq])
   (define table (backend))
   (λ (x) (hash-ref! table x (λ () (f x)))))
 
-(define (memoize2 f #:backend [backend make-hasheq])
+(define (memoize2 f #:backend [backend make-weak-hasheq])
   (define table (backend))
   (λ (x y) (hash-ref! (hash-ref! table x backend) y (λ () (f x y)))))
