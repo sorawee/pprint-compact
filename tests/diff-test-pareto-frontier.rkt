14dcf81c510a09aeefe1ac7de28ff5bff4be3b70
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2021-10-20 20:30:57 -0700

    fix test

--- tests/diff-test-pareto-frontier.rkt
+++ tests/diff-test-pareto-frontier.rkt
@@ -1,39 +1,43 @@
 #lang racket
 
-(module+ test
-  (require rackunit
-           pprint-compact/measure
-           pprint-compact/pareto-frontier)
+(require rackunit
+         "../measure.rkt"
+         "../pareto-frontier.rkt")
 
-  (define (dominated? a b)
-    (and (>= (measure-last-width a) (measure-last-width b))
-         (cond
-           [(= (measure-badness a) (measure-badness b))
-            (>= (measure-height a) (measure-height b))]
-           [else (> (measure-badness a) (measure-badness b))])))
+(define (dominated? a b)
+  (and (>= (measure-last-width a) (measure-last-width b))
+       (cond
+         [(= (measure-badness a) (measure-badness b))
+          (cond
+            [(= (measure-height a) (measure-height b))
+             (>= (measure-cost a) (measure-cost b))]
+            [else
+             (> (measure-height a) (measure-height b))])]
+         [else (> (measure-badness a) (measure-badness b))])))
 
-  (define (naive xs)
-    (let loop ([xs xs] [frontier '()])
-      (match xs
-        ['() frontier]
-        [(cons x xs)
-         (cond
-           [(ormap (λ (front) (dominated? x front)) frontier) (loop xs frontier)]
-           [else (loop xs (cons x (filter-not (λ (front) (dominated? front x)) frontier)))])])))
+(define (naive xs)
+  (let loop ([xs xs] [frontier '()])
+    (match xs
+      ['() frontier]
+      [(cons x xs)
+       (cond
+         [(ormap (λ (front) (dominated? x front)) frontier) (loop xs frontier)]
+         [else (loop xs (cons x (filter-not (λ (front) (dominated? front x)) frontier)))])])))
 
-  (define (gen)
-    (for/list ([i (random 100)])
-      (measure (random 100) (random 100) (random 100) #f)))
+(define (gen)
+  (for/list ([i (random 100)])
+    (measure (random 100) (random 100) (random 100) (random 100) #f)))
 
-  (for ([i (in-range 10000)])
-    (define orig (gen))
-    (define xs (compute-frontier orig))
-    (define ys (naive orig))
+(for ([i (in-range 10000)])
+  (define orig (gen))
+  (define xs (compute-frontier orig))
+  (define ys (naive orig))
 
-    (printf "checking ~a\n" i)
-    (unless (and (equal? (length xs) (length ys))
-                 (equal? (list->set xs) (list->set ys)))
-      (printf "orig: ~a\n" orig)
-      (printf "len: ~ax\n" (length orig))
-      (check-equal? (length xs) (length ys))
-      (check-equal? (list->set xs) (list->set ys)))))
+  (when (zero? (modulo i 1000))
+    (printf "checking ~a\n" i))
+  (unless (and (equal? (length xs) (length ys))
+               (equal? (list->set xs) (list->set ys)))
+    (printf "orig: ~a\n" orig)
+    (printf "len: ~ax\n" (length orig))
+    (check-equal? (length xs) (length ys))
+    (check-equal? (list->set xs) (list->set ys))))
